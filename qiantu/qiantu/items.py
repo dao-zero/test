92f5de383d5376b2c4a2f1d7764ce02585f3372f
xiaoyuan.daima <758855142@qq.com>
2021-09-04 19:34:16 +0800

    master

--- qiantu/qiantu/items.py
+++ qiantu/qiantu/items.py
@@ -9,4 +9,12 @@ import scrapy
 class QiantuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    industry = scrapy.Field()
+    job_name = scrapy.Field()
+    address = scrapy.Field()
+    salary = scrapy.Field()
+    company_name = scrapy.Field()
+    company_type = scrapy.Field()
+    education = scrapy.Field()
+    experience = scrapy.Field()
+    welfare = scrapy.Field()
--- qiantu/qiantu/pipelines.py
+++ qiantu/qiantu/pipelines.py
@@ -6,8 +6,46 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import pymysql
 
 class QiantuPipeline:
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host='localhost',
+            port=3306,
+            user='root',
+            passwd='root',
+            db='qiantu',
+            charset='utf8'
+        )
+        self.cursor = self.connect.cursor()
+
     def process_item(self, item, spider):
-        return item
+        # item = LiepspiderItem()
+        industry = item['industry']
+        job_name = item['job_name']
+        address = item['address']
+        salary = item['salary']
+        company_name = item['company_name']
+        company_type = item['company_type']
+        education = item['education']
+        experience = item['experience']
+        welfare = item['welfare']
+        print(job_name)
+        insert_sql = "INSERT INTO job_second(industry, job_name, address,salary, company_name, company_type" \
+                     ",education, experience, welfare) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                                                                                    industry, job_name, address,
+                                                                                    salary, company_name, company_type,
+                                                                                    education, experience, welfare)
+        # insert_sql = "INSERT INTO job_second(industry, job_name, address,salary, company_name, company_type" \
+        #              ",education, experience, welfare) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+        #                  item['industry'], item['job_name'], item['address'],
+        #                  item['salary'], item['company_name'], item['company_type'],
+        #                  item['education'], item['experience'], item['welfare'])
+
+        self.cursor.execute(insert_sql)
+        self.connect.commit()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connect.close()
